d50ac25d9f2eb9650f31f12d6ab27182275b8f09
zaixuan@123 <2565075424@qq.com>
2022-10-10 22:34:46 +0800

    change project name first commit

---
+++ probability_test/function_test.py
@@ -0,0 +1,13 @@
+import math
+
+# I need it,but I am on a version with 3.9,it's so bad
+
+"""
+import latexify
+@latexify.with_latex#调用latexify的装饰器
+def solve(a, b, c):
+  return (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+
+solve
+print('\pm;')
+"""
